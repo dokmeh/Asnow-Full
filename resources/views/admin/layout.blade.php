f6c4186c51ca49dbd83c97fb9106d1aca4cbb086
Katerou22 <xkaterou22@gmail.com>
2017-01-02 02:19:57 +0330

    Backend Refactoring

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,5 +1,37 @@
 <!DOCTYPE html>
 <html>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+<script>
+    $(window).load(function () {
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut("slow");
+
+    });
+</script>
+<style>
+    .no-js #loader { display : none; }
+
+    .js #loader { display : block; position : absolute; left : 100px; top : 0; }
+
+    .se-pre-con {
+        position   : fixed;
+        left       : 0px;
+        top        : 0px;
+        width      : 100%;
+        height     : 100%;
+        z-index    : 9999;
+        background : url('/img/Preloader_2.gif') center no-repeat #fff;
+        }
+</style>
+<div class="se-pre-con"></div>
+<script>
+    window.onload = function () {
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut("slow");
+
+    };
+</script>
 <head>
     <title>Asnow - Panel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
